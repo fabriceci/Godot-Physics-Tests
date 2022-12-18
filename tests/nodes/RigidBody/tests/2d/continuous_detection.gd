75ed75bcc466c6fc3b6479d7d65ab7c0c367043c
fabriceci <fabricecipolla@gmail.com>
2022-12-18 14:26:28 +0100

    Add extra tests for CCD

--- tests/nodes/RigidBody/tests/2d/continuous_detection.gd
+++ tests/nodes/RigidBody/tests/2d/continuous_detection.gd
@@ -1,7 +1,7 @@
 extends PhysicsUnitTest2D
 
 var speed := 25000
-var simulation_duration := 1
+var simulation_duration := .1
 
 func test_description() -> String:
 	return """Checks if the Continuous Collision Detection (CCD) is working, it must ensure that moving
@@ -11,8 +11,12 @@ func test_description() -> String:
 func test_name() -> String:
 	return "RigidBody | testing Continuous Collision Detection (CCD)"
 
+var detect_x_collision := false
+var detect_y_collision := false
+
 func start() -> void:
-	var vertical_wall = get_static_body_with_collision_shape(Rect2(Vector2(0,0), Vector2(2, Global.WINDOW_SIZE.y/2)), PhysicsTest2D.TestCollisionShape.RECTANGLE, true)
+	
+	var vertical_wall = get_static_body_with_collision_shape(Rect2(Vector2(0,0), Vector2(2, Global.WINDOW_SIZE.y)), PhysicsTest2D.TestCollisionShape.RECTANGLE, true)
 	vertical_wall.position = Vector2(TOP_RIGHT.x - Global.WINDOW_SIZE.y/2 -1, 0)
 	add_child(vertical_wall)
 	
@@ -20,47 +24,38 @@ func start() -> void:
 	horizontal_wall.position = Vector2(BOTTOM_RIGHT.x - Global.WINDOW_SIZE.y/2, BOTTOM_RIGHT.y -50)
 	add_child(horizontal_wall)
 	
-	var rigid_x_ccd_disabled = create_rigid_body(RigidBody2D.CCD_MODE_DISABLED)
-	rigid_x_ccd_disabled.position = Vector2(50, 50)
-	
-	var rigid_x_ccd_ray = create_rigid_body(RigidBody2D.CCD_MODE_CAST_RAY)
+	var rigid_x_ccd_ray := create_rigid_body(RigidBody2D.CCD_MODE_CAST_RAY)
+	rigid_x_ccd_ray.name = "lolilol"
 	rigid_x_ccd_ray.position = Vector2(50, 150)
+	rigid_x_ccd_ray.body_entered.connect(x_collide.bind(rigid_x_ccd_ray))
 	
-	var rigid_x_ccd_shape = create_rigid_body(RigidBody2D.CCD_MODE_CAST_SHAPE)
+	var rigid_x_ccd_shape := create_rigid_body(RigidBody2D.CCD_MODE_CAST_SHAPE)
 	rigid_x_ccd_shape.position = Vector2(50, 250)
 	
-	var rigid_y_ccd_disabled = create_rigid_body(RigidBody2D.CCD_MODE_DISABLED, false)
-	rigid_y_ccd_disabled.position = Vector2(vertical_wall.position.x + 50, 50)
-	
-	var rigid_y_ccd_ray = create_rigid_body(RigidBody2D.CCD_MODE_CAST_RAY, false)
+	var rigid_y_ccd_ray := create_rigid_body(RigidBody2D.CCD_MODE_CAST_RAY, false)
 	rigid_y_ccd_ray.position = Vector2(vertical_wall.position.x + 150, 50)
+	rigid_y_ccd_ray.body_entered.connect(y_collide.bind(rigid_y_ccd_ray))
 	
-	var rigid_y_ccd_shape = create_rigid_body(RigidBody2D.CCD_MODE_CAST_SHAPE, false)
+	var rigid_y_ccd_shape := create_rigid_body(RigidBody2D.CCD_MODE_CAST_SHAPE, false)
 	rigid_y_ccd_shape.position = Vector2(vertical_wall.position.x + 250, 50)
 	
 	var x_lambda = func(p_target, p_monitor):
-		if p_target.continuous_cd == RigidBody2D.CCD_MODE_DISABLED:
-			return p_target.position.x > vertical_wall.position.x # bad
-		else:
-			return p_target.position.x <= vertical_wall.position.x # good
+		return p_target.position.x <= vertical_wall.position.x # good
 
 	var y_lambda = func(p_target, p_monitor):
-		if p_target.continuous_cd == RigidBody2D.CCD_MODE_DISABLED:
-			return p_target.position.y > horizontal_wall.position.y # bad
-		else:
-			return p_target.position.y <= horizontal_wall.position.y # good
+		return p_target.position.y <= horizontal_wall.position.y # good
 	
-	var x_no_ccd_monitor = create_generic_expiration_monitor(rigid_x_ccd_disabled, x_lambda, null, simulation_duration)
-	x_no_ccd_monitor.test_name = "Rigid moving in x without CCD pass through the wall"
+	var collide_x_lambda = func(p_target, p_monitor):
+		return detect_x_collision
+
+	var collide_y_lambda = func(p_target, p_monitor):
+		return detect_y_collision
 
 	var x_ray_ccd_monitor = create_generic_expiration_monitor(rigid_x_ccd_ray, x_lambda, null, simulation_duration)
 	x_ray_ccd_monitor.test_name = "Rigid moving in x with CCD Ray does not pass through the wall"
 	
 	var x_shape_ccd_monitor = create_generic_expiration_monitor(rigid_x_ccd_shape, x_lambda, null, simulation_duration)
 	x_shape_ccd_monitor.test_name = "Rigid moving in x with CCD Cast shape does not pass through the wall"
-	
-	var y_no_ccd_monitor = create_generic_expiration_monitor(rigid_y_ccd_disabled, y_lambda, null, simulation_duration)
-	y_no_ccd_monitor.test_name = "Rigid moving in y without CCD pass through the wall"
 
 	var y_ray_ccd_monitor = create_generic_expiration_monitor(rigid_y_ccd_ray, y_lambda, null, simulation_duration)
 	y_ray_ccd_monitor.test_name = "Rigid moving in y with CCD Ray does not pass through the wall"
@@ -68,16 +63,31 @@ func start() -> void:
 	var y_shape_ccd_monitor = create_generic_expiration_monitor(rigid_y_ccd_shape, y_lambda, null, simulation_duration)
 	y_shape_ccd_monitor.test_name = "Rigid moving in y with CCD Cast shape does not pass through the wall"
 	
+	var x_collision_monitor = create_generic_expiration_monitor(rigid_x_ccd_ray, collide_x_lambda, null, simulation_duration)
+	x_collision_monitor.test_name = "Rigid moving in x with CCD detects collision"
+
+	var y_collision_monitor = create_generic_expiration_monitor(rigid_y_ccd_ray, collide_y_lambda, null, simulation_duration)
+	y_collision_monitor.test_name = "Rigid moving in y with CCD detects collision"
+	
 	process_mode = Node.PROCESS_MODE_DISABLED # to be able to see something
 	await get_tree().create_timer(.5).timeout
 	process_mode = Node.PROCESS_MODE_INHERIT
 
-func create_rigid_body(p_ccd_mode: RigidBody2D.CCDMode, p_horizontal := true, p_shape: PhysicsTest2D.TestCollisionShape = TestCollisionShape.CIRCLE) -> RigidBody2D:
+func create_rigid_body(p_ccd_mode: RigidBody2D.CCDMode, p_horizontal := true, p_shape: PhysicsTest2D.TestCollisionShape = TestCollisionShape.RECTANGLE) -> RigidBody2D:
 	var player = RigidBody2D.new()
 	player.add_child(get_default_collision_shape(p_shape))
 	player.gravity_scale = 0
 	player.continuous_cd = p_ccd_mode
+	player.contact_monitor = true
+	player.max_contacts_reported = 2
+	player.rotation = 90 # Case where the movement vector was not properly being transformed into local space, see #69934
 	var force = Vector2(speed, 0) if p_horizontal else Vector2(0, speed)
 	player.apply_central_impulse(force)
 	add_child(player)
 	return player
+
+func x_collide(_body, _player):
+	detect_x_collision = true
+	
+func y_collide(_body, _player):
+	detect_y_collision = true
--- tests/nodes/RigidBody/tests/3d/continuous_detection.gd
+++ tests/nodes/RigidBody/tests/3d/continuous_detection.gd
@@ -1,7 +1,7 @@
 extends PhysicsUnitTest3D
 
 var speed := 25000
-var simulation_duration := 1
+var simulation_duration := 0.1
 
 func test_description() -> String:
 	return """Checks if the Continuous Collision Detection (CCD) is working, it must ensure that moving
@@ -11,23 +11,30 @@ func test_description() -> String:
 func test_name() -> String:
 	return "RigidBody | testing Continuous Collision Detection (CCD)"
 
+var detect_x_collision := false
+var detect_y_collision := false
+
 func start() -> void:
 	# TO DO: Instanciate scene nodes in code
 	var vertical_mov_wall = $VerticalStaticBody3D
 	var horizontal_mov_wall = $HorizontalStaticBody3D
 		
 	var x_lambda = func(p_target: RigidBody3D, p_monitor):
-		if p_target.continuous_cd == false:
-			return p_target.position.x > horizontal_mov_wall.position.x # bad
-		else:
-			return p_target.position.x <= horizontal_mov_wall.position.x # good
+		return p_target.position.x <= horizontal_mov_wall.position.x # good
 
 	var y_lambda = func(p_target:RigidBody3D , p_monitor):
-		if p_target.continuous_cd == false:
-			return p_target.position.y < vertical_mov_wall.position.y # bad
-		else:
-			return p_target.position.y >= vertical_mov_wall.position.y # good
-			
+		return p_target.position.y >= vertical_mov_wall.position.y # good
+	
+	var collide_x_lambda = func(p_target, p_monitor):
+		return detect_x_collision
+
+	var collide_y_lambda = func(p_target, p_monitor):
+		return detect_y_collision
+	#var horizontal_rigid_body = create_rigid_body(true)
+	#var vertical_rigid_body = create_rigid_body(false)
+	$HorizontalRigidBody3D.body_entered.connect(x_collide.bind($HorizontalRigidBody3D))
+	$VerticalRigidBody3D.body_entered.connect(y_collide.bind($VerticalRigidBody3D))
+	
 	$HorizontalRigidBody3D.apply_central_impulse(Vector3(speed, 0, 0))
 	$VerticalRigidBody3D.apply_central_impulse(Vector3(0, -speed, 0))
 	
@@ -37,6 +44,36 @@ func start() -> void:
 	var y_shape_ccd_monitor = create_generic_expiration_monitor($VerticalRigidBody3D, y_lambda, null, simulation_duration)
 	y_shape_ccd_monitor.test_name = "Rigid moving in y with CCD Cast shape does not pass through the wall"
 	
+	var x_collision_monitor = create_generic_expiration_monitor($HorizontalRigidBody3D, collide_x_lambda, null, simulation_duration)
+	x_collision_monitor.test_name = "Rigid moving in x with CCD detects collision"
+
+	var y_collision_monitor = create_generic_expiration_monitor($VerticalRigidBody3D, collide_y_lambda, null, simulation_duration)
+	y_collision_monitor.test_name = "Rigid moving in y with CCD detects collision"
+	
 	process_mode = Node.PROCESS_MODE_DISABLED # to be able to see something
 	await get_tree().create_timer(.1).timeout
 	process_mode = Node.PROCESS_MODE_INHERIT
+
+func create_rigid_body(p_horizontal := true) -> RigidBody3D:
+	var player = RigidBody3D.new()
+	player.add_child(get_default_collision_shape(PhysicsTest3D.TestCollisionShape.BOX))
+	player.gravity_scale = 0
+	player.continuous_cd = true
+	player.contact_monitor = true
+	player.max_contacts_reported = 2
+	if p_horizontal:		
+		player.rotation = Vector3(0, 45, 45) # Case where the movement vector was not properly being transformed into local space, see #69934
+		player.position = Vector3(-10, 2, 0)
+	else:
+		player.rotation = Vector3(45, 0, 0)
+		player.position = Vector3(8, 8, 0)
+	var force = Vector3(speed, 0, 0) if p_horizontal else Vector3(0, -speed, 0)
+	player.apply_central_impulse(force)
+	add_child(player)
+	return player
+
+func x_collide(_body, _player):
+	detect_x_collision = true
+	
+func y_collide(_body, _player):
+	detect_y_collision = true
