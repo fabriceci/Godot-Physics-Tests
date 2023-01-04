91d040d64eebb136c017ec3a81c5c5283146721f
fabriceci <fabricecipolla@gmail.com>
2023-01-04 18:22:01 +0100

    Fix the sync to physics tilemap test

--- tests/features/moving_platform/tests/2d/sync_to_physics_tilemap.gd
+++ tests/features/moving_platform/tests/2d/sync_to_physics_tilemap.gd
@@ -12,36 +12,36 @@ func test_name() -> String:
 	return "Sync to Physics | testing if [collision_animatable] sync body to the Tilemap2D platform"
 
 func start() -> void:
+	
+	animatable_tilemap.collision_animatable = true
+	
 	# Check x displacement
-	var check_pos_x_callback = func(p_target, p_monitor: Monitor):
+	var check_pos_x_callback = func(p_body: CharacterBody2D, p_monitor: GenericExpirationMonitor):
 		await get_tree().process_frame #sync to physics is applied after the physics frame
-		var body: CharacterBody2D = p_monitor.data["character"]
-		if body.position.x - p_monitor.data["platform"].position.x != 0:
+		var x_diff = p_body.position.x - 25 - p_monitor.data["platform"].position.x 
+		if x_diff > p_body.get_safe_margin() or x_diff < -p_body.get_safe_margin(): # => != 0
 			p_monitor.data["failure"] += 1
 
 	# Check y displacement
-	var check_pos_y_callback = func(p_target, p_monitor: Monitor):
+	var check_pos_y_callback = func(p_body: CharacterBody2D, p_monitor: GenericExpirationMonitor):
 		await get_tree().process_frame #sync to physics is applied after the physics frame
-		var body: CharacterBody2D = p_monitor.data["character"]
-		var y_diff = body.position.y + 25 - p_monitor.data["platform"].position.y # /!\ here 25
-		if y_diff > body.get_safe_margin() or y_diff < -body.get_safe_margin(): # => != 0
+		var y_diff = p_body.position.y + 25 - p_monitor.data["platform"].position.y # /!\ here 25
+		if y_diff > p_body.get_safe_margin() or y_diff < -p_body.get_safe_margin(): # => != 0
 			p_monitor.data["failure"] += 1
 		
-	var test_lambda = func(p_target, p_monitor):
+	var test_lambda = func(p_target, p_monitor: GenericExpirationMonitor):
 		if not p_monitor.data["failure"] == 0:
-			p_monitor.error_message = "Out of sync during %d frames" % [p_monitor.data["failure"]]
+			p_monitor.error_message = "Out of sync during %d/%d frames" % [p_monitor.data["failure"], p_monitor.frame]
 		return p_monitor.data["failure"] == 0
 		
 	var start_pos: Vector2 = animatable_tilemap.position
 	var end_pos := start_pos + displacement
 	
-	animatable_tilemap.collision_animatable = true
-	var character := create_character(CENTER - Vector2(-25, 25))
+	var character := create_character(CENTER + Vector2(25, -25))
 	var tween: Tween = get_tree().create_tween()
 	tween.set_process_mode(Tween.TWEEN_PROCESS_PHYSICS)
 	tween.tween_property(animatable_tilemap, "position", end_pos, simulation_duration/2)
 	tween.tween_property(animatable_tilemap, "position", start_pos, simulation_duration/2)
-
 	create_sync_to_physics_monitor("pos x", animatable_tilemap, character, test_lambda, check_pos_x_callback)
 	create_sync_to_physics_monitor("pos y", animatable_tilemap, character, test_lambda, check_pos_y_callback)
 
@@ -49,7 +49,6 @@ func create_sync_to_physics_monitor(p_axis: String, p_animator: Node, p_characte
 	var monitor = create_generic_expiration_monitor(p_character, p_test, p_cbk, simulation_duration)
 	monitor.test_name = "Tilemap with [sync_to_physics] %s is sync" % [p_axis]
 	monitor.data["platform"] = p_animator
-	monitor.data["character"] = p_character
 	monitor.data["failure"] = 0
 	monitor.success = true
 	return monitor
