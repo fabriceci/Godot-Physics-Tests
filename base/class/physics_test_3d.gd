1c6d7ce8d8a1537ff85a837637b33bde203cfe23
Ricardo Buring <ricardo.buring@gmail.com>
2023-01-11 22:38:46 +0100

    Add tests for CharacterBody3D sliding along static bodies

--- base/class/physics_test_3d.gd
+++ base/class/physics_test_3d.gd
@@ -18,6 +18,7 @@ enum TestCollisionShape {
 	CONVEX_POLYGON_ULTRA_HIGH_VERTEX = 101,
 	BOX = PhysicsServer3D.SHAPE_BOX,
 	SPHERE = PhysicsServer3D.SHAPE_SPHERE,
+	CYLINDER = PhysicsServer3D.SHAPE_CYLINDER,
 }
 
 func _ready() -> void:
@@ -53,7 +54,6 @@ func get_static_body_with_collision_shape(p_shape_definition, p_shape_type := Te
 	body.add_child(body_col)
 	return body
 
-# Get CollisionShape2D or CollisionPolygon2D that fits in the rectangle.
 func get_collision_shape(p_shape_definition, p_shape_type := TestCollisionShape.BOX) -> Node3D:
 	var col = null
 	if p_shape_type == TestCollisionShape.CAPSULE:
@@ -62,13 +62,19 @@ func get_collision_shape(p_shape_definition, p_shape_type := TestCollisionShape.
 		col.shape = CapsuleShape3D.new()
 		col.shape.radius = p_shape_definition.x
 		col.shape.height = p_shape_definition.y
+	elif p_shape_type == TestCollisionShape.CYLINDER:
+		assert(p_shape_definition is Vector2, "Shape definition for a Cylinder must be a Vector2")
+		col = CollisionShape3D.new()
+		col.shape = CylinderShape3D.new()
+		col.shape.radius = p_shape_definition.x
+		col.shape.height = p_shape_definition.y
 	elif p_shape_type == TestCollisionShape.SPHERE:
-		assert(p_shape_definition is float, "Shape definition for a Circle must be a float")
+		assert(p_shape_definition is float, "Shape definition for a Sphere must be a float")
 		col = CollisionShape3D.new()
 		col.shape = SphereShape3D.new()
 		col.shape.radius = p_shape_definition
 	elif p_shape_type == TestCollisionShape.BOX:
-		assert(p_shape_definition is Vector3, "Shape definition for a Rectangle must be a Rect2")
+		assert(p_shape_definition is Vector3, "Shape definition for a Box must be a Vector3")
 		col = CollisionShape3D.new()
 		col.shape = BoxShape3D.new()
 		col.shape.size = p_shape_definition
@@ -80,8 +86,7 @@ func get_collision_shape(p_shape_definition, p_shape_type := TestCollisionShape.
 		col.shape = cached_high_poly_convex
 	elif p_shape_type == TestCollisionShape.CONVEX_POLYGON_ULTRA_HIGH_VERTEX:
 		col = CollisionShape3D.new()
-		col.shape = cached_ultra_high_poly_convex		
-		
+		col.shape = cached_ultra_high_poly_convex
 	elif p_shape_type == TestCollisionShape.CONVEX_POLYGON:
 		col = CollisionShape3D.new()
 		col.shape = cached_poly_convex
@@ -97,6 +102,8 @@ func get_default_shape_definition(p_shape_type : TestCollisionShape, p_scale := 
 		return 0.5 * p_scale
 	if p_shape_type == TestCollisionShape.CAPSULE:
 		return Vector2(1,2) * p_scale
+	if p_shape_type == TestCollisionShape.CYLINDER:
+		return Vector2(.5, 1.0) * p_scale
 	if p_shape_type == TestCollisionShape.CONVEX_POLYGON_MEDIUM_VERTEX  or p_shape_type == TestCollisionShape.CONVEX_POLYGON_HIGH_VERTEX or p_shape_type == TestCollisionShape.CONVEX_POLYGON or p_shape_type == TestCollisionShape.CONVEX_POLYGON_ULTRA_HIGH_VERTEX:
 		return null
 	
@@ -106,6 +113,7 @@ func get_default_shape_definition(p_shape_type : TestCollisionShape, p_scale := 
 static func shape_name(p_shape_type : TestCollisionShape) -> String:
 	match p_shape_type:
 		TestCollisionShape.CAPSULE: return "Capsule"
+		TestCollisionShape.CYLINDER: return "Cylinder"
 		TestCollisionShape.CONCAVE_POLYGON: return "Concave Polygon"
 		TestCollisionShape.CONVEX_POLYGON: return "Convex 8v"
 		TestCollisionShape.CONVEX_POLYGON_MEDIUM_VERTEX: return "Convex 146v"
