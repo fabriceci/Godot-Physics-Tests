a81cd4854597488c0d5f13c1a2aa77bc32d26891
Sp3ctralCat <sp3ctralcat@gmail.com>
2023-02-12 12:47:32 -0700

    Fix util equality functions
    Typo + more readable

--- base/Utils.gd
+++ base/Utils.gd
@@ -1,10 +1,10 @@
 extends Node
 
 func vec2_equals(v1: Vector2, v2: Vector2, tolerance := 0.00001) -> bool:
-	return (v1.x - tolerance) <= v2.x and (v1.x + tolerance) >= v2.x and (v1.y - tolerance) <= v2.y and (v1.y + tolerance) >= v2.y
+	return f_equals(v1.x, v2.x, tolerance) and f_equals(v1.y, v2.y, tolerance)
 
 func vec3_equals(v1: Vector3, v2: Vector3, tolerance := 0.00001) -> bool:
-	return (v1.x - tolerance) <= v2.x and (v1.x + tolerance) >= v2.x and (v1.y - tolerance) <= v2.y and (v1.y + tolerance) >= v2.y and (v1.z - tolerance) <= v2.z and (v1.z + tolerance) >= v2.z
+	return f_equals(v1.x, v2.x, tolerance) and f_equals(v1.y, v2.y, tolerance) and f_equals(v1.z, v2.z, tolerance)
 
 func f_equals(f1: float, f2: float, tolerance := 0.00001) -> bool:
-	return (f1 - tolerance) <= f1 and (f1 + tolerance) >= f2 
+	return abs(f1 - f2) <= tolerance
